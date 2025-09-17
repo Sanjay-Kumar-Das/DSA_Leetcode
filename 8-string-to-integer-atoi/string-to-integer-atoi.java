cb1c182bb5b4811bba9e524618ffae06878867ae
Sanjay-Kumar-Das <skdas9114@gmail.com>
2025-09-18 00:01:49 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (14.42%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -2,27 +2,24 @@ class Solution {
     public int myAtoi(String s) {
         s = s.trim();
         if(s.length()==0) return 0;
-        StringBuilder sb = new StringBuilder();
         Boolean neg = false;
         int idx = 0;
         if (s.charAt(idx) == '-' || s.charAt(idx) == '+') {
             neg = (s.charAt(idx) == '-');
             idx++;
         }
+        long ans = 0;
         for(int i=idx;i<s.length();i++){
             char ch = s.charAt(i);
-            if(ch >='0' && ch <='9')sb.append(ch);
-            else break;
+            if(Character.isDigit(ch)){
+               ans = ans*10+ (ch-'0');
+            if(neg==true && -ans < Integer.MIN_VALUE)
+             return Integer.MIN_VALUE;
+            if(neg == false && ans > Integer.MAX_VALUE )
+             return Integer.MAX_VALUE; 
+            }
+             else break; 
         }
-        if(sb.isEmpty()) return 0;
-        String str = sb.toString();
-        try {
-            int x = Integer.parseInt(str);
-            return (neg == true)? 0-x : x;
-        }catch(Exception e){
-            if(neg == true) return Integer.MIN_VALUE;
-            else return Integer.MAX_VALUE;
-        }
-        
+        return neg ? - (int) ans : (int) ans;   
     }
 }
