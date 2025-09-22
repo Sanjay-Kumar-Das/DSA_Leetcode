f61946116db150eae4e02f8ea83a0c3b2e4fc883
Sanjay-Kumar-Das <skdas9114@gmail.com>
2025-09-23 01:14:58 +0530

    Time: 1 ms (81.06%) | Memory: 42.9 MB (82.22%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,16 +1,25 @@
 class Solution {
     static List <String> str ;
-    public void printParanthesis(int open,int close,int n,String s){
-        if(s.length() == n*2){
-            str.add(s);
+    public void printParanthesis(int open,int close,int n,StringBuilder sb){
+        if(sb.length() == n*2){
+            str.add(sb.toString());
             return;
         }
-        if(open < n) printParanthesis(open+1,close,n,s+"(");
-        if(close < open) printParanthesis(open,close+1,n,s+")");
+        if(open < n){
+            sb.append("(");
+            printParanthesis(open+1,close,n,sb);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if(close < open){
+            sb.append(")");
+            printParanthesis(open,close+1,n,sb);
+            sb.deleteCharAt(sb.length()-1);
+        }
     }
     public List<String> generateParenthesis(int n) {
         str = new ArrayList<>();
-        printParanthesis(0,0,n,"");
+        StringBuilder sb = new StringBuilder();
+        printParanthesis(0,0,n,sb);
         return str;
     }
 }
