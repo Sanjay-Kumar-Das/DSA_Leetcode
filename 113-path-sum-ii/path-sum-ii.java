152b62498ed37e481331bf10720e2c3d86f20a93
Sanjay-Kumar-Das <skdas9114@gmail.com>
2025-10-21 23:44:40 +0530

    Time: 2 ms (11.22%) | Memory: 47.7 MB (5.05%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -2,9 +2,9 @@ class Solution {
     public static void findPath(TreeNode root,List<Integer> list, List<List<Integer>>ans,int sum){
         if(root == null) return;
         if(root.left == null && root.right == null){
-            if(sum-root.val == 0){
+            if(sum == root.val){
                 list.add(root.val);
-                ans.add(list);
+                ans.add(new ArrayList<>(list));
             }
             return;
         }
