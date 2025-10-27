104c9b319819c120734ce2a0b63495fc2177b53d
Sanjay-Kumar-Das <skdas9114@gmail.com>
2025-10-28 00:00:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (55.55%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -1,16 +1,11 @@
 class Solution {
-    public static void inorder(TreeNode root,List<TreeNode>arr){
-        if(root == null) return;
-        inorder(root.left,arr);
-        arr.add(root);
-        inorder(root.right,arr);
-    }
+    int sum =0;
     public TreeNode bstToGst(TreeNode root) {
-       List<TreeNode> arr = new ArrayList<>();
-       inorder(root,arr);
-       for(int i=arr.size()-2;i>=0;i--){
-            arr.get(i).val += arr.get(i+1).val;
-       }
+       if(root == null) return null;
+       bstToGst(root.right);
+       sum += root.val;
+       root.val = sum;
+       bstToGst(root.left);
        return root;
     }
 }
