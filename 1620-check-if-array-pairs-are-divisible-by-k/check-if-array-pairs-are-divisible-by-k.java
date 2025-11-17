ca8158c580151d7dc0736dbeed3755d79d29b05d
Sanjay-Kumar-Das <skdas9114@gmail.com>
2025-11-17 23:17:56 +0530

    Time: 37 ms (43.74%) | Memory: 98.5 MB (5.01%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
@@ -5,12 +5,6 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             int ele = ((arr[i] % k)+k)%k;
             map.put(ele,map.getOrDefault(ele,0)+1);
-            // if(map.containsKey(ele)){
-            //     int freq = map.get(ele);
-            //     map.put(ele,freq+1);
-            // }else{
-            //     map.put(ele,1);
-            // }
         }
 
         if(map.containsKey(0)){
@@ -18,11 +12,11 @@ class Solution {
             map.remove(0);
             }
         for(int key : map.keySet()){
-            int rem = k-key;
-            if(!map.containsKey(rem)) return false;
-            int keyfreq = map.get(key);
-            int remfreq = map.get(rem);
-            if(keyfreq != remfreq) return false;
+            // int rem = k-key;
+            if(!map.containsKey(k-key)) return false;
+            //int keyfreq = map.get(key);
+            int remfreq = map.get(k-key);
+            if(map.get(key) != remfreq) return false;
         }
         return true;
     }
